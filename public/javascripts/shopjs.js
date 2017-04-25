9d39dafeba7a5b6c62f6780bfe46cecbc5958110
InvisibleSentry <a1592366@163.com>
2017-04-25 09:08:53 +0800

    go streen
    new change

---
+++ public/javascripts/shopjs.js
@@ -0,0 +1,11 @@
+/**
+ * Created by SJay on 2017/4/23.
+ */
+$(function(){
+    $('.map').click(function(){
+        $("#gaodeMap").css("display","block")
+    })
+    $('#gaodeClose').click(function(){
+        $("#gaodeMap").css("display","none")
+    })
+})
--- public/javascripts/tongcehngbang.js
+++ public/javascripts/tongcehngbang.js
@@ -2,11 +2,20 @@
  * Created by SJay on 2017/4/23.
  */
 $(function(){
-    console.log($("#map"));
     $('#map').click(function(){
         $("#gaodeMap").css("display","block")
     })
     $('#gaodeClose').click(function(){
         $("#gaodeMap").css("display","none")
     })
+
+    $('#changeCityBtn').click(function(){
+        $("#changeCity").css("display","block")
+    })
+    $("#closeBtn").click(function(){
+        $("#changeCity").css("display","none")
+    })
+
+
+
 })
